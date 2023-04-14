e125a98af465f351ad203e0c9325094919166bd9
Kabi <gadalkabi@gmail.com>
2023-04-14 12:30:44 +0530

    Made text printing formate with hacker effect

---
+++ index.js
@@ -0,0 +1,24 @@
+const letters = "ABCDEFGIJKLMNOPQRSTUVWXYZ";
+
+document.querySelector("h1").onmouseover= event =>{
+  let iterations = 0;
+
+  const interval = setInterval(()=>{
+  event.target.innerText = event.target.innerText.split("")
+  .map((letter,index) => {
+    if(index < iterations){
+      return event.target.dataset.value[index];
+    }
+    
+    return letters[Math.floor(Math.random() * 24)]
+  })
+  .join("");
+ 
+  if(iterations >= event.target.dataset.value.length){
+    clearInterval(interval);
+  } 
+
+  iterations += 1;
+},90)
+}
+
